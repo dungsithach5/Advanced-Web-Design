6f1a0ca618f4833ce6214953cad544d186a54efb
dungsithach5 <159418327+dungsithach5@users.noreply.github.com>
2024-10-01 11:08:17 +0700

    change name var

--- hw3/female.js
+++ hw3/female.js
@@ -2,29 +2,29 @@ var female = [
     {
         id:"KO0002",
         prdname:"Quần Jean nữ",
-        sell:269.000,
-        orgprice:370.000,
+        discount:269.000,
+        price:370.000,
         url:"img/QuanJeansNu.jpg",
     },
     {
         id:"VN0002",
         prdname:"Áo khoác nữ",
-        sell:249.000,
-        orgprice:369.000,
+        discount:249.000,
+        price:369.000,
         url:"img/AoKhoacNu.jpg",
     },
     {
         id:"UK0002",
         prdname:"Áo Hoodie nữ",
-        sell:159.000,
-        orgprice:390.000,
+        discount:159.000,
+        price:390.000,
         url:"img/AoHoodieNu.jpg",
     },
     {
         id:"US0002",
         prdname:"Áo phông nữ",
-        sell:159.000,
-        orgprice:390.000,
+        discount:159.000,
+        price:390.000,
         url:"img/AoPhongNu.jpg",
     },
     
@@ -40,7 +40,7 @@ for(var i in female) {
             </div>
             <div class="flex flex-col p-4 space-y-5">
                 <span class="text-xl font-semibold">${female[i].prdname} - ${female[i].id}</span>
-                <span class="text-lg">${female[i].sell.toFixed(3)} vnđ - <s>${female[i].orgprice.toFixed(3)} vnđ</s></span>
+                <span class="text-lg">${female[i].discount.toFixed(3)} vnđ - <s>${female[i].price.toFixed(3)} vnđ</s></span>
                 <button class="mr-auto text-lg text-white bg-red-600 rounded p-1 px-4 hover:opacity-80">Đặt Mua</button>
             </div>
         </div>
--- hw3/male.js
+++ hw3/male.js
@@ -2,29 +2,29 @@ var male = [
     {
         id:"KO0002",
         prdname:"Quần short Khaki (Đen)",
-        sell:269.000,
-        orgprice:250.000,
+        discount:269.000,
+        price:250.000,
         url:"img/QuanShortKhakiDen.jpg",
     },
     {
         id:"VN0002",
         prdname:"Quần baggy đen sang trọng",
-        sell:249.000,
-        orgprice:398.000,
+        discount:249.000,
+        price:398.000,
         url:"img/QuanBaggy.jpg",
     },
     {
         id:"UK0002",
         prdname:"Quần dài khaki",
-        sell:159.000,
-        orgprice:300.000,
+        discount:159.000,
+        price:300.000,
         url:"img/QuanDaiKhaki.jpg",
     },
     {
         id:"US0002",
         prdname:"Quần short Khaki",
-        sell:159.000,
-        orgprice:300.000,
+        discount:159.000,
+        price:300.000,
         url:"img/QuanShortKhaki.jpg",
     },
     
@@ -40,7 +40,7 @@ for(var i in male) {
             </div>
             <div class="flex flex-col p-4 space-y-5">
                 <span class="text-xl font-semibold">${male[i].prdname} </span>
-                <span class="text-lg">${male[i].orgprice.toFixed(3)} vnđ</span>
+                <span class="text-lg">${male[i].price.toFixed(3)} vnđ</span>
                 <button class="mr-auto text-lg text-white bg-red-600 rounded p-1 px-4 hover:opacity-80">Đặt Mua</button>
             </div>
         </div>
